c485103726794eac20a6c53341a7eb6b441b017a
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2023-04-20 21:04:12 +0100

    SecurityConfig  securityFilterChain

--- src/main/java/io/gitlab/markcrowe/furniture/shop/app/SecurityConfig.java
+++ src/main/java/io/gitlab/markcrowe/furniture/shop/app/SecurityConfig.java
@@ -5,12 +5,15 @@ import io.gitlab.markcrowe.furniture.shop.app.services.SimpleUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 
@@ -18,6 +21,16 @@ import java.util.ArrayList;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products").permitAll()
+                .and()
+                .formLogin()// This is the line that generates a login page
+                .defaultSuccessUrl("/products");//	The page to go when you log in
+        return httpSecurity.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder)
