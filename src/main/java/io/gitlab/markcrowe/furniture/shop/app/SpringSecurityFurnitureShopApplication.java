e6f0d7623e36e6e3b921bef940a0b90c258bf312
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2023-04-20 19:25:54 +0100

    starter code

---
+++ src/main/java/io/gitlab/markcrowe/furniture/shop/app/SpringSecurityFurnitureShopApplication.java
@@ -0,0 +1,13 @@
+package io.gitlab.markcrowe.furniture.shop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFurnitureShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityFurnitureShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gitlab/markcrowe/furniture/shop/app/SpringSecurityFurnitureShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitlab.markcrowe.furniture.shop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFurnitureShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
