fb98a44822b239d108bfce2a639ddd330260d9ad
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2023-04-20 20:01:48 +0100

    Insert Product
    Add Validation to POM
    Removed  th:action="@{/add}" from product-insert.html
    Wired the Post Method for inserting a product

--- src/main/java/io/gitlab/markcrowe/furniture/shop/app/web/controllers/ProductWebController.java
+++ src/main/java/io/gitlab/markcrowe/furniture/shop/app/web/controllers/ProductWebController.java
@@ -2,8 +2,10 @@ package io.gitlab.markcrowe.furniture.shop.app.web.controllers;
 
 import io.gitlab.markcrowe.furniture.shop.app.model.Product;
 import io.gitlab.markcrowe.furniture.shop.app.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,5 +31,16 @@ public class ProductWebController {
         var modelAndView = new ModelAndView("product-insert");
         modelAndView.addObject("product", new Product());
         return modelAndView;
+    }
+
+    @RequestMapping("add")
+    public String getInsertProduct(@Valid Product product, BindingResult bindingResult, ModelAndView model)
+    {
+        if(bindingResult.hasErrors())
+            return "product-insert";
+
+        productService.addAProduct(product);
+
+        return "redirect:/products";
     }
 }
