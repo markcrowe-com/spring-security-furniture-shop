d2a3eb50e005a4c4df53168640e151cbb08fd482
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2023-04-21 20:06:57 +0100

    deleteProductByCode

--- src/main/java/io/gitlab/markcrowe/furniture/shop/app/web/controllers/ProductWebController.java
+++ src/main/java/io/gitlab/markcrowe/furniture/shop/app/web/controllers/ProductWebController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,6 +43,13 @@ public class ProductWebController {
 
         productService.addAProduct(product);
 
+        return "redirect:/products";
+    }
+
+    @GetMapping("{code}/delete")
+    public String deleteProductByCode(@PathVariable("code") String code)
+    {
+        productService.deleteAProduct(code);
         return "redirect:/products";
     }
 }
