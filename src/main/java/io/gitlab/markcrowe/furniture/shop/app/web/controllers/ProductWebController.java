5f88abf4538e06b55122cfa2d51ba5337a2b20e1
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2023-04-21 19:38:16 +0100

    Fixed PostMapping

--- src/main/java/io/gitlab/markcrowe/furniture/shop/app/web/controllers/ProductWebController.java
+++ src/main/java/io/gitlab/markcrowe/furniture/shop/app/web/controllers/ProductWebController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,7 +34,7 @@ public class ProductWebController {
         return modelAndView;
     }
 
-    @RequestMapping("add")
+    @PostMapping("add")
     public String getInsertProduct(@Valid Product product, BindingResult bindingResult, ModelAndView model)
     {
         if(bindingResult.hasErrors())
